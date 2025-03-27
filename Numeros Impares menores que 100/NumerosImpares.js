a46066a9ff39c88725c9d52f2bfa3226f9b89ebf
Lucas Flin <malucasnba@gmail.com>
2025-03-27 19:27:21 -0300

    Adicionei comentários para facilitar a leitura.

--- Numeros Impares menores que 100/NumerosImpares.js
+++ Numeros Impares menores que 100/NumerosImpares.js
@@ -1,10 +1,12 @@
 // O Desafio é fazer um programa que mostra todos os numeros impares menores que 100
 // Creditos: https://github.com/giselle-ferreira/ExerciciosJavascriptResolvidos?tab=readme-ov-file#questões
 
+//O programa explica para o usuário o que ele vai mostrar
 console.log("Esses são todos os números ímpares menores que 100:")
 
+//Criado um loop que repetirá o codigo e aumentará o contador até ele chegar no 100
 for (let i = 1;i < 100; i++){
-     if (i % 2 != 0){
+     if (i % 2 != 0){ //Se o numero do contador for impar (resto da divisão por 2 não for 0) mostra o número
         console.log(i)
-     }
+     }//Se for, nada acontece e o loop se repete
 }
